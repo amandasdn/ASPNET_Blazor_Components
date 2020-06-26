148cbf9d6a8a97c5e2ebe940612ca2ea8d569bd0
Amanda Nascimento <asdn.amanda@gmail.com>
2020-06-25 23:02:07 -0300

    Field text/number

---
+++ BlazorComponents/BlazorComponents.Examples/Pages/Forms.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponents.Examples.Pages
+{
+    public class FormsModel : LayoutComponentBase
+    {
+        public class PersonExample
+        {
+            [Display(Name = "Nome")]
+            public string Name { get; set; }
+
+            [Display(Name = "Sobrenome")]
+            public string Surname { get; set; }
+
+            [Display(Name = "Idade")]
+            public int Age { get; set; }
+
+            [Display(Name = "Data de Nascimento")]
+            public DateTime Birthday { get; set; }
+        }
+
+        public PersonExample PersonObj = new PersonExample
+        {
+            Name = "Amanda",
+            Surname = "Nascimento",
+            Age = (DateTime.Now.Year - 1999),
+            Birthday = new DateTime(1999, 06, 20)
+        };
+    }
+}
