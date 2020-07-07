87b74908ed33eb5ed081cff22dc808ac52b78d51
Amanda Nascimento <asdn.amanda@gmail.com>
2020-07-06 23:19:01 -0300

    Modal component

---
+++ BlazorComponents/BlazorComponents.Examples/Pages/General.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorComponents.Examples.Components;
+
+namespace BlazorComponents.Examples.Pages
+{
+    public class GeneralModel : LayoutComponentBase
+    {
+        public Modal ModalExample;
+    }
+}
