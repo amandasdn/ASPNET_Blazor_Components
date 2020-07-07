437dd492754b0d8c569a6a6837284ea34076aa6f
Amanda Nascimento <asdn.amanda@gmail.com>
2020-07-07 16:17:25 -0300

    Lock scroll by js (modal)

--- BlazorComponents/BlazorComponents.Examples/wwwroot/js/main.js
+++ BlazorComponents/BlazorComponents.Examples/wwwroot/js/main.js
@@ -1,1 +1,13 @@
-﻿
+﻿// ====================================================================
+// Modal
+
+function lockScrollBody(isHidden) {
+    if (isHidden) {
+        $("body").removeClass(" scroll-locked");
+    }
+    else {
+        $("body").addClass(" scroll-locked");
+    }
+}
+
+// ====================================================================
